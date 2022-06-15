9b7785bb37d3ca01cae9cad0cbb485451323695a
lalat <lalatkishorebehera2014@gmail.com>
2022-06-15 17:37:11 +0530

    first commit

---
+++ stringClass.java
@@ -0,0 +1,10 @@
+class stringClass
+{
+    public static void main(String[] args) 
+    {
+        String str = "Lalat";
+        System.out.println(str);
+        String str2 = str.concat(" kishore");
+        System.out.println(str2);
+    }
+}
