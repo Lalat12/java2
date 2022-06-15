db1d862bfb3b75bc332e51ade403159ae0f30eb9
lalat <lalatkishorebehera2014@gmail.com>
2022-06-15 16:32:31 +0530

    first commit

---
+++ This1.java
@@ -0,0 +1,19 @@
+class This1
+{
+    int a;
+    This1()
+    {
+    this(15);
+    }
+    
+    This1(int a)
+    {
+        
+        System.out.println(a);
+
+    }
+    public static void main(String[] args) {
+        This1 r=new This1();
+        
+    }
+}
